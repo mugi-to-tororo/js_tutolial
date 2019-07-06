9501bbbf0b8bbbaad9095e7013bbd71176e4c519
miya <info.miya38@gmail.com>
2019-07-06 19:03:15 +0900

    done!

--- 11/zipcode/practice/js/app.js
+++ 11/zipcode/practice/js/app.js
@@ -1,2 +1,25 @@
+$(function() {
+    $('#btn').on('click', function() {
+        //webapiに住所情報をリクエストする
+        $.ajax({
+            url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#zipcode').val(),
+            dataType: 'jsonp',
+        }).done(function(data) {
+            if (data.results) {
+                setData(data.results[0])
+            } else {
+                alert('該当するデータは見つかりませんでした。')
+            }
+        }).fail(function(data) {
+            alert('通信に失敗しました。')
+        });
+    });
 
+    //データ取得後の処理
+    function setData(data) {
+        $('#prefecture').val(data.address1);
+        $('#city').val(data.address2);
+        $('#address').val(data.address3);
+    }
+});
 
